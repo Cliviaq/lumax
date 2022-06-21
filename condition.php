84150c533a0b3751d11fc2719862ef35c25edc64
cliviaq <quliahmwas@gmail.com>
2022-06-21 04:43:49 -0700

    blue

--- condition.php
+++ condition.php
@@ -1,2 +1,58 @@
 <?php
-
+$no4=45;
+$no9=78;
+if($no4>8) {
+    echo "yes its true";
+}
+echo "<br>";
+if($no9>90){
+    echo "yeah";
+}else{
+    echo "no";
+}
+echo "<br>";
+$marks=65;
+if ($marks<39){
+    echo "fail";
+}
+else if ($marks>=40 && $marks<49) {
+    echo "D grade";
+}
+else if ($marks>=50 && $marks<60) {
+    echo "C grade";
+}
+else if ($marks>=61 && $marks<74) {
+    echo "B grade";
+}
+else if ($marks>=75 && $marks<90) {
+    echo "A grade";
+}
+else if ($marks>=90 && $marks<100) {
+    echo "A+ grade";
+}
+else {
+    echo "Invalid input";
+}
+echo "<br>";
+$marks=75;
+if ($marks<39){
+    echo "fail";
+}
+else if ($marks>=40 && $marks<49) {
+    echo "D grade";
+}
+else if ($marks>=50 && $marks<60) {
+    echo "C grade";
+}
+else if ($marks>=61 && $marks<74) {
+    echo "B grade";
+}
+else if ($marks>=75 && $marks<90) {
+    echo "A grade";
+}
+else if ($marks>=90 && $marks<100) {
+    echo "A+ grade";
+}
+else {
+    echo "Invalid input";
+}
