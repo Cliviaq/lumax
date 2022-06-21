c6c1ca030706266f1e4f54565baed869f889d805
cliviaq <quliahmwas@gmail.com>
2022-06-21 05:36:31 -0700

    blue

---
+++ assingment.php
@@ -0,0 +1,20 @@
+<?php
+$no=60;
+switch ($no){
+
+    case "33":
+        echo"less than the no";
+        break;
+    case "79":
+        echo"more than the no";
+        break;
+
+    case "68":
+        echo"close to the no";
+        break;
+
+    case "60":
+        echo"Same as the number.";
+        break;
+
+}
--- switch.php
+++ switch.php
@@ -1,1 +1,23 @@
 <?php
+$car="Audi";
+switch ($car){
+
+    case "Toyota":
+        echo "My car ie not a toyota :)";
+        break;
+    case "Honda":
+         echo "Nope still not a Honda  ;)";
+         break;
+    case "Audi":
+        echo "Actually this is my car.";
+        break;
+    default:
+        echo "not in the list.";
+        break;
+    case"Subaru":
+        echo "The noise this car produces...";
+        break;
+    case "BMW":
+        echo " This is also a nice car.";
+        break;
+}
